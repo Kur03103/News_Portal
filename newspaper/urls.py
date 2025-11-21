a767f5e25396818c19278d637fc727e7123699df
Kur03103 <aayushaj3103@gmail.com>
2025-11-21 16:38:01 +0545

    Updated on Latest New, Pagination

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HomeView
-
+from . import views
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("post-list/", views.PostListView.as_view(), name="post-list"),
+    path("post-detail/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
 ]
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,39 +1,65 @@
 from django.shortcuts import render
-
-from newspaper.models import Post
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.utils import timezone
 from datetime import timedelta
+from .models import Post
 
 # Create your views here.
-
 class HomeView(TemplateView):
     template_name = "newsportal/home.html"
     
-    # if we want to send extra data to the template we use get_context_data
+    # If we want to add extra context data to our template we use get_context_data
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         context["breaking_news"] = Post.objects.filter(
-            published_at__isnull=False, status = "active", is_breaking_news=True
+            is_breaking_news=True,
+            published_at__isnull=False,
+            status="active"
         ).order_by("-published_at")[:3]
 
         context["featured_post"] = (
-            Post.objects.filter(published_at__isnull= False, status= "active")
-        ).order_by("-published_at", "-views_count").first()
-
+            Post.objects.filter(published_at__isnull=False, status="active")
+            .order_by("-published_at", "views_count")
+            .first()
+        )
         context["trending_news"] = Post.objects.filter(
-            published_at__isnull= False, status = "active"
+            published_at__isnull=False,
+            status="active"
         ).order_by("-published_at")[:4]
 
         one_week_ago = timezone.now() - timedelta(days=7)
         context["weekly_top_posts"] = Post.objects.filter(
-            published_at__isnull=False, status = "active", published_at__gte=one_week_ago
+            published_at__isnull=False, status="active", published_at__gte=one_week_ago
         ).order_by("-published_at", "-views_count")[:5]
-
         context["popular_posts"] = Post.objects.filter(
-            published_at__isnull=False, status = "active"
+            published_at__isnull=False,
+            status="active"
         ).order_by("-published_at")[:5]
-
-
         return context
+class PostListView(ListView):
+    model = Post
+    template_name = "newsportal/list/list.html"
+    context_object_name = "posts"
+    paginate_by = 1
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            published_at__isnull=False,
+            status="active"
+        ).order_by("-published_at")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["popular_posts"] = Post.objects.filter(
+            published_at__isnull=False,
+            status="active"
+        ).order_by("-published_at")[:5]
+        return context
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "newsportal/detail/detail.html"
+    context_object_name = "post"
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        query = query.filter(published_at__isnull=False, status="active")
+        return query
